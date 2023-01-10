f1cf47eb777368480b97ff8fd3d8c615ed03eb21
VladanStar <vladancupric@gmail.com>
2023-01-10 19:32:13 +0100

    Add

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-
-mainL = document.getElementById("mainL")
-
-
 $(document).ready(function () {
+    mainL = document.getElementById("mainL");
+mainL.style.display = "none";
 
-mainL.style.display = "none"
-
+let link = "https://services.odata.org/V3/Northwind/Northwind.svc/Customers";
+let orderLink = "https://services.odata.org/V3/Northwind/Northwind.svc/Order_Details";
+let productsLink = "https://services.odata.org/V3/Northwind/Northwind.svc/Products";
+let customers;
 
 
 })
